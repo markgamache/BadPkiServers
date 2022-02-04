4d7fd1f918a349cce3e6f7026c99a1d1f78d223f
Mark R. Gamache <mark.gamache@gmail.com>
2022-02-04 10:32:13 -0800

    test client settings c

--- buildConf.ps1
+++ buildConf.ps1
@@ -191,10 +191,13 @@ server {
     ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
     ssl_prefer_server_ciphers off;
     ssl_verify_client       on;
-    ssl_trusted_certificate /etc/nginx/pki/Gamache Client ICA/cert.pem;
+    ssl_trusted_certificate /etc/nginx/pki/Gamache Trust Root 2018/cert.pem;
     ssl_verify_depth 0;
 
     add_header Strict-Transport-Security "max-age=45" always;
+    if ($ssl_client_verify != SUCCESS) {
+       return 403;
+    }
 
 
 }
@@ -222,12 +225,14 @@ server {
     ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
     ssl_prefer_server_ciphers off;
     ssl_verify_client       on;
-    ssl_client_certificate /etc/nginx/pki/Gamache Client ICA/cert.pem;
-    ssl_trusted_certificate /etc/nginx/pki/Gamache Client ICA/cert.pem;
+    ssl_client_certificate /etc/nginx/pki/Gamache Trust Root 2018/cert.pem;
+    ssl_trusted_certificate /etc/nginx/pki/Gamache Trust Root 2018/cert.pem;
     ssl_verify_depth 0;
 
     add_header Strict-Transport-Security "max-age=45" always;
-
+    if ($ssl_client_verify != SUCCESS) {
+       return 403;
+    }
 
 }
     
