bfe9ed207ecf16690310009fcc0c414cece7efca
Mark R. Gamache <mark.gamache@gmail.com>
2022-02-09 14:16:31 -0800

    final MTLS?? B

--- buildConf.ps1
+++ buildConf.ps1
@@ -266,7 +266,7 @@ server {
     ssl_ciphers ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:DHE-RSA-AES128-GCM-SHA256:DHE-RSA-AES256-GCM-SHA384;
     ssl_prefer_server_ciphers off;
     ssl_verify_client       on;
-    ssl_trusted_certificate /etc/nginx/pki/clientVer.pem;
+    ssl_client_certificate /etc/nginx/pki/clientVer.pem;
     #ssl_trusted_certificate /etc/nginx/pki/Gamache Trust Root 2018/cert.pem;
     ssl_verify_depth 3;
     
