5531b7889cced88196a79bf3fea51365849b0aa0
Mark R. Gamache <mark.gamache@gmail.com>
2022-02-11 14:55:16 -0800

    maybe SHA1

--- intCertRollOut.ps1
+++ intCertRollOut.ps1
@@ -86,9 +86,27 @@ $baseHTTP = "http://pki.pkilab.markgamache.com/"
         Copy-Item -Force $crlBack.basePath "$($artifacts)/$($certBack.serial).crl"
         
 
-        # whittlebury.pkilab.markgamache.com .  issuer has SHA1
-            $did = & python3 ./DoCAStuff.py --mode NewLeafTLS --basepath $baseP --name "whittlebury.pkilab.markgamache.com" --signer "Gamache Int CA 1776" --validfrom dtMinusTenMin --validto dtPlusOneYear --keysize 2048
-            $did | ConvertFrom-Json
+        # Gamache Issuer 1776
+        $did = & python3 ./DoCAStuff.py --mode NewSubCA --basepath $baseP --name "Gamache Issuer 1776" --signer "Gamache Int CA 1776" --validfrom dtMinusTenMin --validto janOf2028 --keysize 2048 --pathlength 0
+        $certBack = $did | ConvertFrom-Json
+        $intCA = $certBack
+
+        #AIA
+        "$($baseHTTP)$($certBack.serial).crt" | Out-File -FilePath "$($certBack.basePath)/aia.txt" -Encoding ascii -NoNewline
+        Copy-Item -Force  "$($certBack.DERFile)" "$($artifacts)/$($certBack.serial).crt"
+
+        #crl  
+        "badf00d" | Out-File -FilePath "$($certBack.basePath)/revoked.txt"  -Encoding ascii
+        "$($baseHTTP)$($certBack.serial).crl" | Out-File -FilePath "$($certBack.basePath)/cdp.txt" -Encoding ascii -NoNewline
+        $did = & python3 ./DoCAStuff.py --mode SignCRL --basepath $baseP --signer "Gamache Issuer 1776" --validfrom dtMinusTenMin --validto dtPlusOneYear 
+        $crlBack = $did | ConvertFrom-Json
+        Copy-Item -Force $crlBack.basePath "$($artifacts)/$($certBack.serial).crl"
+
+
+
+            # whittlebury.pkilab.markgamache.com .  works create just another demo
+                $did = & python3 ./DoCAStuff.py --mode NewLeafTLS --basepath $baseP --name "whittlebury.pkilab.markgamache.com" --signer "Gamache Issuer 1776" --validfrom dtMinusTenMin --validto dtPlusOneYear --keysize 2048
+                $did | ConvertFrom-Json
 
 
     # Gamache Int CA 2018 
